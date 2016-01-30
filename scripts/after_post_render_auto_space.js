60f30ef159e146f9c172110aecd4b6751819a5a7
whiledoing <whiledoing@163.com>
2016-01-30 13:39:48 +0800

    add pangu module : for auto spacing

---
+++ scripts/after_post_render_auto_space.js
@@ -0,0 +1,10 @@
+/**
+ * auto space after post render : using [pangu](https://github.com/vinta/pangu.js)
+ */
+
+var pangu = require('pangu');
+
+hexo.extend.filter.register('after_post_render', function(data) {
+    data.title = pangu.spacing(data.title);
+    data.content = pangu.spacing(data.content);
+});
