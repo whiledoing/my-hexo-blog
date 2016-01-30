d42e890c8c8881c176816efcc648220b136e8591
whiledoing <whiledoing@163.com>
2016-01-30 14:39:52 +0800

    auto-spacing for toc

--- scripts/after_post_render_auto_space.js
+++ scripts/after_post_render_auto_space.js
@@ -7,4 +7,7 @@ var pangu = require('pangu');
 hexo.extend.filter.register('after_post_render', function(data) {
     data.title = pangu.spacing(data.title);
     data.content = pangu.spacing(data.content);
+    
+    /* for toc */
+    data.excerpt = pangu.spacing(data.excerpt);
 });
